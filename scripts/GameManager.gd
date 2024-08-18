5e008b38acbb89ccc4f2b27eff09329a839d7587
Nir Ben Aderet <nir.ben.aderet@joytunes.com>
2024-08-18 11:53:28 +0300

    support changing levels

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -30,6 +30,41 @@ var sfx_player: AudioStreamPlayer
 
 signal beat_signal
 
+
+
+func load_rhythmic_pattern_level() -> void:
+	var rhythm_game_level: RhythmGameLevel = RhythmGameLevel.new("res://rhythmGameLevelExampleLevel2.json")
+	var stages: Dictionary = rhythm_game_level.get_stage(1)
+	# Assuming stages["notes"] contains the list of notes
+	var input_notes: Array[Dictionary] = []
+	if "notes" in stages:
+		for note: Dictionary in stages["notes"]:
+			input_notes.append(note)
+			
+			
+	for i in range(input_notes.size()):
+		var type: String  = "note"
+		if input_notes[i]["is_rest"]:
+			type = "rest"
+		notes_dictionary[i] = {
+			"x_location": i * note_quarter_gap,
+			"duration": "quarter",  # Assuming all are quarter notes, adjust as needed
+			"type": type,
+			"status": note_status.IDLE,  # Assuming note_status is defined elsewhere in your code
+		}
+		notes[i].set_type(type)
+	
+	notes_dictionary[0]["status"] = note_status.ACTIVE
+	
+	var count: int = 0
+	for note in notes:
+		note.position.x = notes_dictionary[count]["x_location"]
+		note.position.y = note_y_location
+		count += 1
+	print(notes_dictionary)
+	
+	
+	
 func _ready() -> void:
 	sfx_player = MusicPlayer.get_child(0)
 	if not MusicPlayer.playing:
@@ -161,6 +196,7 @@ func pulse(note_num: int) -> void:
 	notes[note_num].position.y += note_highlight_offset
 
 func restart_level() -> void:
+	load_rhythmic_pattern_level()
 	#if instruction.text == "Listen...":
 		#instruction.text = "Play!"
 	#else:
--- scripts/note.gd
+++ scripts/note.gd
@@ -6,3 +6,23 @@ var path_rest_quarter: String = "res://rest_quarter.png"
 func _ready() -> void:
 	if type == "rest":
 		texture = load(path_rest_quarter)
+
+
+# Setter function for the type attribute
+func set_type(value: String) -> void:
+	type = value
+	load_image_for_type(type)
+	
+	
+# Function to load and display an image based on the type
+func load_image_for_type(image_type: String) -> void:
+	var image_path: String = ""
+	match image_type:
+		"rest":
+			image_path = path_rest_quarter
+		"Quarter":
+			image_path = "res://quarter-note.png"
+		_:
+			image_path = "res://quarter-note.png"
+	texture = load(image_path)
+
